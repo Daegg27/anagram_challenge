3e8582d58c9566f4cc3b112553ac3e5a2d15a3bc
Dalton Eggleston <egglestondalton@yahoo.com>
2022-05-26 18:32:40 -0700

    Did it!

--- js/characterMatch.js
+++ js/characterMatch.js
@@ -1,3 +1,20 @@
 exports.isCharacterMatch = function(string1, string2) {
 
+    let sortedStr1 = string1.toLowerCase().split("").sort().join("");
+    let sortedStr2 = string2.toLowerCase().split("").sort().join("");
+    console.log(sortedStr1);
+    console.log(sortedStr2);
+
+    if (string1.length !== string2.length)
+    {
+        return false;
+    }
+    else if (sortedStr1 == sortedStr2)
+    {
+        return true;
+    }else{
+        return false;
+    }
+
 };
+// console.log(exports.isCharacterMatch("adkwo", "kido"));
--- js/characterMatchSpec.js
+++ js/characterMatchSpec.js
@@ -2,10 +2,10 @@
 
 var ana = require("./characterMatch");
 
-console.log(ana.isCharacterMatch('charm', 'march') === true);
-console.log(ana.isCharacterMatch('zach', 'attack') === false);
-console.log(ana.isCharacterMatch('CharM', 'mARcH') === true);
-console.log(ana.isCharacterMatch('abcde2', 'c2abed') === true);
+console.log(ana.isCharacterMatch('charm', 'march'));
+console.log(ana.isCharacterMatch('zach', 'attack'));
+console.log(ana.isCharacterMatch('CharM', 'mARcH'));
+console.log(ana.isCharacterMatch('abcde2', 'c2abed'));
 
 console.log("This test is for the challenge quesiton");
 console.log(ana.isCharacterMatch('Anna Madrigal', 'A man and a girl') === true);
